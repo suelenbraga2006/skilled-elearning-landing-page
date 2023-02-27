02cc3df810d8382439046261f97feb1b392b9695
Suelen Braga <suelen.braga2006@gmail.com>
2023-02-27 17:51:49 -0300

    Ajustes finais feitos

--- src/App.js
+++ src/App.js
@@ -5,13 +5,47 @@ import Courses from './components/Courses/Courses';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
+const courses = [
+  {
+    id: 1,
+    title: 'Animation',
+    text: 'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
+    icon: 'animation',
+  },
+  {
+    id: 2,
+    title: 'Design',
+    text: 'Create beautiful, usable interfaces to help shape the future of how the web looks.',
+    icon: 'design',
+  },
+  {
+    id: 3,
+    title: 'Photography',
+    text: 'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
+    icon: 'photography',
+  },
+
+  {
+    id: 4,
+    title: 'Crypto',
+    text: 'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
+    icon: 'crypto',
+  },
+  {
+    id: 5,
+    title: 'Business',
+    text: 'A step-by-step playbook to help you start, scale, and sustain your business without outside investment.',
+    icon: 'business',
+  },
+];
+
 function App() {
   return (
     <>
       <Header />
       <main>
         <Banner />
-        <Courses />
+        <Courses courses={courses} />
       </main>
       <Footer />
     </>
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,44 +1,25 @@
 import React from 'react';
 
-import animation from '../../assets/images/icon-animation.svg';
 import './styles.scss';
 
-function Courses() {
+function Courses({ courses }) {
   return (
     <section className="courses">
       <article className="highlight">
         <h2>Check out our most popular courses!</h2>
       </article>
 
-      <article>
-        <img src={animation} alt="Animation" />
-        <h3>Animation</h3>
-        <p>
-          Learn the latest animation techniques to create stunning motion design
-          and captivate your audience.
-        </p>
-        <a href="/getstarted">Get Started</a>
-      </article>
-
-      <article>
-        <img src={animation} alt="Animation" />
-        <h3>Animation</h3>
-        <p>
-          Learn the latest animation techniques to create stunning motion design
-          and captivate your audience.
-        </p>
-        <a href="/getstarted">Get Started</a>
-      </article>
-
-      <article>
-        <img src={animation} alt="Animation" />
-        <h3>Animation</h3>
-        <p>
-          Learn the latest animation techniques to create stunning motion design
-          and captivate your audience.
-        </p>
-        <a href="/getstarted">Get Started</a>
-      </article>
+      {courses.map((course) => (
+        <article key={course.id}>
+          <img
+            src={require(`../../assets/images/icon-${course.icon}.svg`)}
+            alt={course.title}
+          />
+          <h3>{course.title}</h3>
+          <p>{course.text}</p>
+          <a href="/getstarted">Get Started</a>
+        </article>
+      ))}
     </section>
   );
 }
