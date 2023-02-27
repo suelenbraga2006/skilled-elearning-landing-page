e00506d04ab4c37fab67eb08896ed152cd9ccc0b
Suelen Braga <suelen.braga2006@gmail.com>
2023-02-27 18:11:33 -0300

    Tentativa de arrumar erro no Vercel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './assets/styles/global.scss';
 
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner/banner';
 import Courses from './components/Courses/Courses';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
